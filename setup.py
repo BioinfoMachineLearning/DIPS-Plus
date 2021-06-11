8d97b0decc35144b973a6e42c21f95b25c20d4c2
Alex Morehead <alex.morehead@gmail.com>
2021-06-10 20:52:39 -0500

    Add PyPI required packages

--- setup.py
+++ setup.py
@@ -4,11 +4,11 @@ from setuptools import setup, find_packages
 
 setup(
     name='DIPS-Plus',
-    version='0.0.1',
+    version='0.0.2',
     description='The Enhanced Database of Interacting Protein Structures for Interface Prediction',
     author='Alex Morehead',
     author_email='alex.morehead@gmail.com',
     url='https://github.com/amorehead/DIPS-Plus',
-    install_requires=[''],
+    install_requires=['setuptools', 'dill', 'tqdm', 'easy-parallel-py3', 'atom3-py3', 'click'],
     packages=find_packages(),
 )

5077f12acc9fcd41b63400639ce8ea1704cfe6c4
Alex Morehead <alex.morehead@gmail.com>
2021-09-17 10:43:18 -0500

    Add CASP-CAPRI parallel processing scripts

--- project/datasets/builder/compile_casp_capri_dataset_on_andes.sh
+++ project/datasets/builder/compile_casp_capri_dataset_on_andes.sh
@@ -3,7 +3,7 @@
 ####################### Batch Headers #########################
 #SBATCH -A BIF135
 #SBATCH -p batch
-#SBATCH -J make_evcoupling_dataset
+#SBATCH -J make_casp_capri_dataset
 #SBATCH -t 0-24:00
 #SBATCH --mem 224G
 #SBATCH --nodes 4
@@ -28,6 +28,6 @@ export HHSUITE_DB=/gpfs/alpine/scratch/$USER/bif132/Data/Databases/bfd_metaclust
 # Run dataset compilation scripts
 cd "$PROJDIR"/project || exit
 
-srun python3 "$PROJDIR"/project/datasets/builder/generate_hhsuite_features.py "$PROJDIR"/project/datasets/EVCoupling/interim/parsed "$PROJDIR"/project/datasets/EVCoupling/interim/parsed "$HHSUITE_DB" "$PROJDIR"/project/datasets/EVCoupling/interim/external_feats --rank "$1" --size "$2" --num_cpu_jobs 4 --num_cpus_per_job 8 --num_iter 2 --source_type evcoupling --read_file
+srun python3 "$PROJDIR"/project/datasets/builder/generate_hhsuite_features.py "$PROJDIR"/project/datasets/CASP-CAPRI/interim/parsed "$PROJDIR"/project/datasets/CASP-CAPRI/interim/parsed "$HHSUITE_DB" "$PROJDIR"/project/datasets/CASP-CAPRI/interim/external_feats --rank "$1" --size "$2" --num_cpu_jobs 4 --num_cpus_per_job 8 --num_iter 2 --source_type casp_capri --write_file
 
-#srun python3 "$PROJDIR"/project/datasets/builder/postprocess_pruned_pairs.py "$PROJDIR"/project/datasets/EVCoupling/raw "$PROJDIR"/project/datasets/EVCoupling/interim/pairs "$PROJDIR"/project/datasets/EVCoupling/interim/external_feats "$PROJDIR"/project/datasets/EVCoupling/final/raw --num_cpus 32 --rank "$1" --size "$2" --source_type EVCoupling
+#srun python3 "$PROJDIR"/project/datasets/builder/postprocess_pruned_pairs.py "$PROJDIR"/project/datasets/CASP-CAPRI/raw "$PROJDIR"/project/datasets/CASP-CAPRI/interim/pairs "$PROJDIR"/project/datasets/CASP-CAPRI/interim/external_feats "$PROJDIR"/project/datasets/CASP-CAPRI/final/raw --num_cpus 32 --rank "$1" --size "$2" --source_type CASP-CAPRI
--- project/datasets/builder/launch_parallel_slurm_jobs_on_andes.sh
+++ project/datasets/builder/launch_parallel_slurm_jobs_on_andes.sh
@@ -9,25 +9,29 @@ BFD_COPY_IDS=("_1" "_2" "_3" "_4" "_5" "_6" "_7" "_8" "_9" "_10" "_11" "_12" "_1
 NUM_BFD_COPIES=${#BFD_COPY_IDS[@]}
 
 # Whether to launch multi-node compilation jobs for DIPS or instead for DB5
-compile_dips=true
+compile_dips=false
+compile_db5=false
+compile_evcoupling=false
+compile_casp_capri=true
 
 if [ "$compile_dips" = true ]; then
   # Job 1 - DIPS
-  echo Submitting job 1 for compile_evcoupling_dataset_on_andes.sh with parameters: 0 "$NUM_BFD_COPIES"
-  sbatch "$PROJDIR"/project/datasets/builder/compile_evcoupling_dataset_on_andes.sh 0 "$NUM_BFD_COPIES"
+  echo Submitting job 1 for compile_dips_dataset_on_andes.sh with parameters: 0 "$NUM_BFD_COPIES"
+  sbatch "$PROJDIR"/project/datasets/builder/compile_dips_dataset_on_andes.sh 0 "$NUM_BFD_COPIES"
 
   # Job 2 - DIPS
-  echo Submitting job 2 for compile_evcoupling_dataset_on_andes.sh with parameters: 1 "$NUM_BFD_COPIES"
-  sbatch "$PROJDIR"/project/datasets/builder/compile_evcoupling_dataset_on_andes.sh 1 "$NUM_BFD_COPIES"
+  echo Submitting job 2 for compile_dips_dataset_on_andes.sh with parameters: 1 "$NUM_BFD_COPIES"
+  sbatch "$PROJDIR"/project/datasets/builder/compile_dips_dataset_on_andes.sh 1 "$NUM_BFD_COPIES"
 
   # Job 3 - DIPS
-  echo Submitting job 3 for compile_evcoupling_dataset_on_andes.sh with parameters: 2 "$NUM_BFD_COPIES"
-  sbatch "$PROJDIR"/project/datasets/builder/compile_evcoupling_dataset_on_andes.sh 2 "$NUM_BFD_COPIES"
+  echo Submitting job 3 for compile_dips_dataset_on_andes.sh with parameters: 2 "$NUM_BFD_COPIES"
+  sbatch "$PROJDIR"/project/datasets/builder/compile_dips_dataset_on_andes.sh 2 "$NUM_BFD_COPIES"
 
   # Job 4 - DIPS
-  echo Submitting job 4 for compile_evcoupling_dataset_on_andes.sh with parameters: 3 "$NUM_BFD_COPIES"
-  sbatch "$PROJDIR"/project/datasets/builder/compile_evcoupling_dataset_on_andes.sh 3 "$NUM_BFD_COPIES"
-else
+  echo Submitting job 4 for compile_dips_dataset_on_andes.sh with parameters: 3 "$NUM_BFD_COPIES"
+  sbatch "$PROJDIR"/project/datasets/builder/compile_dips_dataset_on_andes.sh 3 "$NUM_BFD_COPIES"
+
+elif [ "$compile_db5" = true ]; then
   # Job 1 - DB5
   echo Submitting job 1 for compile_db5_dataset_on_andes.sh with parameters: 0 "$NUM_BFD_COPIES"
   sbatch "$PROJDIR"/project/datasets/builder/compile_db5_dataset_on_andes.sh 0 "$NUM_BFD_COPIES"
@@ -43,4 +47,55 @@ else
   # Job 4 - DB5
   echo Submitting job 4 for compile_db5_dataset_on_andes.sh with parameters: 3 "$NUM_BFD_COPIES"
   sbatch "$PROJDIR"/project/datasets/builder/compile_db5_dataset_on_andes.sh 3 "$NUM_BFD_COPIES"
+
+elif [ "$compile_evcoupling" = true ]; then
+  # Job 1 - EVCoupling
+  echo Submitting job 1 for compile_evcoupling_dataset_on_andes.sh with parameters: 0 "$NUM_BFD_COPIES"
+  sbatch "$PROJDIR"/project/datasets/builder/compile_evcoupling_dataset_on_andes.sh 0 "$NUM_BFD_COPIES"
+
+  # Job 2 - EVCoupling
+  echo Submitting job 2 for compile_evcoupling_dataset_on_andes.sh with parameters: 1 "$NUM_BFD_COPIES"
+  sbatch "$PROJDIR"/project/datasets/builder/compile_evcoupling_dataset_on_andes.sh 1 "$NUM_BFD_COPIES"
+
+  # Job 3 - EVCoupling
+  echo Submitting job 3 for compile_evcoupling_dataset_on_andes.sh with parameters: 2 "$NUM_BFD_COPIES"
+  sbatch "$PROJDIR"/project/datasets/builder/compile_evcoupling_dataset_on_andes.sh 2 "$NUM_BFD_COPIES"
+
+  # Job 4 - EVCoupling
+  echo Submitting job 4 for compile_evcoupling_dataset_on_andes.sh with parameters: 3 "$NUM_BFD_COPIES"
+  sbatch "$PROJDIR"/project/datasets/builder/compile_evcoupling_dataset_on_andes.sh 3 "$NUM_BFD_COPIES"
+
+elif [ "$compile_casp_capri" = true ]; then
+  # Job 1 - CASP-CAPRI
+  echo Submitting job 1 for compile_casp_capri_dataset_on_andes.sh with parameters: 0 "$NUM_BFD_COPIES"
+  sbatch "$PROJDIR"/project/datasets/builder/compile_casp_capri_dataset_on_andes.sh 0 "$NUM_BFD_COPIES"
+
+  # Job 2 - CASP-CAPRI
+  echo Submitting job 2 for compile_casp_capri_dataset_on_andes.sh with parameters: 1 "$NUM_BFD_COPIES"
+  sbatch "$PROJDIR"/project/datasets/builder/compile_casp_capri_dataset_on_andes.sh 1 "$NUM_BFD_COPIES"
+
+  # Job 3 - CASP-CAPRI
+  echo Submitting job 3 for compile_casp_capri_dataset_on_andes.sh with parameters: 2 "$NUM_BFD_COPIES"
+  sbatch "$PROJDIR"/project/datasets/builder/compile_casp_capri_dataset_on_andes.sh 2 "$NUM_BFD_COPIES"
+
+  # Job 4 - CASP-CAPRI
+  echo Submitting job 4 for compile_casp_capri_dataset_on_andes.sh with parameters: 3 "$NUM_BFD_COPIES"
+  sbatch "$PROJDIR"/project/datasets/builder/compile_casp_capri_dataset_on_andes.sh 3 "$NUM_BFD_COPIES"
+
+else
+  # Job 1 - DIPS
+  echo Submitting job 1 for compile_dips_dataset_on_andes.sh with parameters: 0 "$NUM_BFD_COPIES"
+  sbatch "$PROJDIR"/project/datasets/builder/compile_dips_dataset_on_andes.sh 0 "$NUM_BFD_COPIES"
+
+  # Job 2 - DIPS
+  echo Submitting job 2 for compile_dips_dataset_on_andes.sh with parameters: 1 "$NUM_BFD_COPIES"
+  sbatch "$PROJDIR"/project/datasets/builder/compile_dips_dataset_on_andes.sh 1 "$NUM_BFD_COPIES"
+
+  # Job 3 - DIPS
+  echo Submitting job 3 for compile_dips_dataset_on_andes.sh with parameters: 2 "$NUM_BFD_COPIES"
+  sbatch "$PROJDIR"/project/datasets/builder/compile_dips_dataset_on_andes.sh 2 "$NUM_BFD_COPIES"
+
+  # Job 4 - DIPS
+  echo Submitting job 4 for compile_dips_dataset_on_andes.sh with parameters: 3 "$NUM_BFD_COPIES"
+  sbatch "$PROJDIR"/project/datasets/builder/compile_dips_dataset_on_andes.sh 3 "$NUM_BFD_COPIES"
 fi

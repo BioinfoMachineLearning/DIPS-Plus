a43ab524cade7e714a4e8c605d6167b13e9de452
Alex Morehead <alex.morehead@gmail.com>
2021-10-07 11:20:23 -0500

    Upgrade atom3-py3 version listed to gain access to PSAIA error code handling feature

--- setup.py
+++ setup.py
@@ -4,11 +4,20 @@ from setuptools import setup, find_packages
 
 setup(
     name='DIPS-Plus',
-    version='0.0.7',
+    version='0.0.8',
     description='The Enhanced Database of Interacting Protein Structures for Interface Prediction',
     author='Alex Morehead',
-    author_email='alex.morehead@gmail.com',
-    url='https://github.com/amorehead/DIPS-Plus',
-    install_requires=['setuptools', 'dill', 'tqdm', 'easy-parallel-py3', 'atom3-py3', 'click'],
+    author_email='acmwhb@umsystem.edu',
+    url='https://github.com/BioinfoMachineLearning/DIPS-Plus',
+    install_requires=[
+        'setuptools==56.2.0',
+        'dill==0.3.3',
+        'tqdm==4.49.0',
+        'Sphinx==4.0.1',
+        'easy-parallel-py3==0.1.6.4',
+        'atom3-py3==0.1.9.8',
+        'click==7.0.0',
+        # mpi4py==3.0.3  # On Andes, do 'source venv/bin/activate', 'module load gcc/10.3.0', and 'pip install mpi4py --no-cache-dir --no-binary :all:'
+    ],
     packages=find_packages(),
 )
